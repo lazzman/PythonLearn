1441fdd86427b8feeb4249648eef6bc3eeb70700
nodcat <173966404@qq.com>
2017-08-20 15:03:58 +0800

    修改

--- Python3_Learn/基础/字符串.py
+++ Python3_Learn/基础/字符串.py
@@ -7,6 +7,17 @@ print('I\'m \"OK\"!', type('I\'m \"OK\"!'))
 # 不转义字符
 print(r"\n\d\t 都是转义字符", type(r"\n\d\t 都是转义字符"))
 
+# 输出多行文本
+print("""
+如果字符串内部有很多换行，用\n写在一行里不好阅读，为了简化，Python允许用'''...'''的格式表示多行内容，可以自己试试：
+>>> print('''line1
+line2
+line3''')
+line1
+line2
+line3
+""")
+
 s = 'Python-中文'
 print(s, type(s))
 
