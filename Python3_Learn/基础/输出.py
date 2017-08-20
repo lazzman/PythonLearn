b5c41d30c6c58a0b9cd157fe5babf3e60e1b6325
nodcat <173966404@qq.com>
2017-08-20 15:13:23 +0800

    修改

--- Python3_Learn/基础/输出.py
+++ Python3_Learn/基础/输出.py
@@ -5,3 +5,6 @@ print('The quick brown fox', 'jumps over', 'the lazy dog')
 print(300)
 print(100 + 200)
 print('100 + 200 =', 100 + 200)
+print("修改字符串默认分隔符", "修改成冒号", sep="：")
+print("c语言占位符形式输出：%d %0.3f %s %x" % (100, 1.1e6, 'hello', 255))
+
