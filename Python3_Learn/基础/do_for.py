edd77452abd3b44fb6397b023fbfedaff350b172
nodcat <173966404@qq.com>
2017-08-20 23:29:40 +0800

    修改

--- Python3_Learn/基础/do_for.py
+++ Python3_Learn/基础/do_for.py
@@ -8,4 +8,21 @@ for name in names:
 
 # 打印数字 0 - 9
 for x in range(10):
+    # 到7终止循环2
+    if x == 7:
+        break
+    # 跳过5
+    if x == 5:
+        continue
     print(x)
+else:
+    print("else")
+
+
+'''
+range对象实现了__iter__方法，而list的构造方法可以传入一个可迭代对象来构造一个list对象
+list(iterable) -> new list initialized from iterable's items
+'''
+a = range(1, 6)
+print(type(a))
+print(list(a))
--- Python3_Learn/基础/do_while.py
+++ Python3_Learn/基础/do_while.py
@@ -15,4 +15,15 @@ n = 1
 while n <= 100:
     acc = acc * n
     n = n + 1
+else:
+    print("else")
+
 print(acc)
+
+# 打印0-9之间的奇数
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:  # 如果n是偶数，执行continue语句
+        continue  # continue语句会直接继续下一轮循环，后续的print()语句不会执行
+    print(n)
