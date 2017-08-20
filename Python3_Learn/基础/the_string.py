7842818885518a34cac6d439c5f107b273d2a325
nodcat <173966404@qq.com>
2017-08-20 14:43:39 +0800

    修改

--- Python3_Learn/基础/the_string.py
+++ Python3_Learn/基础/the_string.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # 转义字符
-print('I\'m \"OK\"!')
+print('I\'m \"OK\"!', type('I\'m \"OK\"!'))
 
 # 不转义字符
-print(r"\n\d\t 都是转义字符")
+print(r"\n\d\t 都是转义字符", type(r"\n\d\t 都是转义字符"))
 
 s = 'Python-中文'
-print(s)
+print(s, type(s))
 
-# unicode转换为utf-8编码输出
+# unicode转换为utf-8编码的字节数组形式输出
 b = s.encode('utf-8')
-print(b)
+print(b, type(b))
 
-# 将utf-8字符串解码为unicode
+# 将utf-8字节数组解码为unicode
 print(b.decode('utf-8'))
