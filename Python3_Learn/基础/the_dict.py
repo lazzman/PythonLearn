e147ac85dd92b753e675c4ac34d4c152979b3c4e
nodcat <173966404@qq.com>
2017-08-21 00:19:00 +0800

    修改

--- Python3_Learn/基础/the_dict.py
+++ Python3_Learn/基础/the_dict.py
@@ -43,3 +43,29 @@ print("dict =", d)
 print("dict have name?", "name" in d)
 print("dict.get(key,defaultValue)", d.get("name"))
 print("dict.get(key,defaultValue)", d.get("name", "没有找到"))
+
+# 删除一个key，用pop(key)方法，对应的value也会从dict中删除
+print("dict.pop(key)", d.pop("Adam"), d)
+
+'''
+请务必注意，dict内部存放的顺序和key放入的顺序是没有关系的。
+
+和list比较，dict有以下几个特点：
+查找和插入的速度极快，不会随着key的增加而变慢；
+需要占用大量的内存，内存浪费多。
+
+而list相反：
+查找和插入的时间随着元素的增加而增加；
+占用空间小，浪费内存很少。
+
+所以，dict是用空间来换取时间的一种方法。
+dict可以用在需要高速查找的很多地方，在Python代码中几乎无处不在，正确使用dict非常重要，需要牢记的第一条就是dict的key必须是不可变对象。
+这是因为dict根据key来计算value的存储位置，如果每次计算相同的key得出的结果不同，那dict内部就完全混乱了。这个通过key计算位置的算法称为哈希算法（Hash）。
+要保证hash的正确性，作为key的对象就不能变。在Python中，字符串、整数等都是不可变的，因此，可以放心地作为key。
+而list是可变的，就不能作为key。根本原因在于list没有实现hash方法，只有实现了__hash__方法的类才可以作为key
+set list map 都不能作为key
+'''
+# key = [1, 2, 3] # TypeError: unhashable type: 'list'
+key = (1, 2, 3)
+# key = {'a': 1} # TypeError: unhashable type: 'dict'
+d[key] = 'a list cannot be the key , but tuple can'
--- Python3_Learn/基础/the_set.py
+++ Python3_Learn/基础/the_set.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+set和dict类似，也是一组key的集合，但不存储value。由于key不能重复，所以，在set中，没有重复的key
+
+'''
+
 s1 = set([1, 1, 2, 2, 3, 3])
 print(s1)
+
+# set可以看成数学意义上的无序和无重复元素的集合，因此，两个set可以做数学意义上的交集、并集等操作
 s2 = set([2, 3, 4])
 print(s1 & s2)
 print(s1 | s2)
+
+# 通过add(key)方法可以添加元素到set中，可以重复添加，但不会有效果
+print("s2.add(4)", s2.add(4), s2)
+
+# 通过remove(key)方法可以删除元素
+print("s2.remove(4)", s2.remove(4), s2)
