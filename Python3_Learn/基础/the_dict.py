1e5873f42b06724c01adbe74cfc8c5f70bb6baa7
nodcat <17089793114@163.com>
2017-08-26 16:40:15 +0800

    修改错误说明

--- Python3_Learn/基础/the_dict.py
+++ Python3_Learn/基础/the_dict.py
@@ -62,7 +62,7 @@ print("dict.pop(key)", d.pop("Adam"), d)
 dict可以用在需要高速查找的很多地方，在Python代码中几乎无处不在，正确使用dict非常重要，需要牢记的第一条就是dict的key必须是不可变对象。
 这是因为dict根据key来计算value的存储位置，如果每次计算相同的key得出的结果不同，那dict内部就完全混乱了。这个通过key计算位置的算法称为哈希算法（Hash）。
 要保证hash的正确性，作为key的对象就不能变。在Python中，字符串、整数等都是不可变的，因此，可以放心地作为key。
-而list是可变的，就不能作为key。根本原因在于list没有实现hash方法，只有实现了__hash__方法的类才可以作为key
+而list是可变的，就不能作为key。
 set list map 都不能作为key
 '''
 # key = [1, 2, 3] # TypeError: unhashable type: 'list'
