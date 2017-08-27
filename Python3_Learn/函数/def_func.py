8ca36c6a56916fabd91b399f48506c1dd22591f0
nodcat <17089793114@163.com>
2017-08-27 23:12:08 +0800

    修改错误说明

--- Python3_Learn/函数/def_func.py
+++ Python3_Learn/函数/def_func.py
@@ -39,3 +39,10 @@ print(defaultChangeParam([123]))  # [123, 'END']
 print(defaultChangeParam())  # ['END']
 print(defaultChangeParam())  # ['END', 'END']
 print(defaultChangeParam())  # ['END', 'END', 'END']
+'''
+
+Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
+
+所以，定义默认参数要牢记一点：默认参数必须指向不变对象！
+
+'''
