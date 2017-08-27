f978578af9eb4514de4c9d2f1a10051353354719
nodcat <17089793114@163.com>
2017-08-27 23:10:42 +0800

    修改错误说明

--- Python3_Learn/函数/def_func.py
+++ Python3_Learn/函数/def_func.py
@@ -3,6 +3,7 @@
 
 import math
 
+
 def my_abs(x):
     if not isinstance(x, (int, float)):
         raise TypeError('bad operand type')
@@ -11,10 +12,12 @@ def my_abs(x):
     else:
         return -x
 
+
 def move(x, y, step, angle=0):
     nx = x + step * math.cos(angle)
     ny = y - step * math.sin(angle)
     return nx, ny
+
 
 n = my_abs(-20)
 print(n)
@@ -22,5 +25,17 @@ print(n)
 x, y = move(100, 100, 60, math.pi / 6)
 print(x, y)
 
+
 # TypeError: bad operand type:
-my_abs('123')
+# my_abs('123')
+
+# 默认参数 - 可变对象特殊情景
+def defaultChangeParam(L=[]):
+    L.append('END')
+    return L
+
+
+print(defaultChangeParam([123]))  # [123, 'END']
+print(defaultChangeParam())  # ['END']
+print(defaultChangeParam())  # ['END', 'END']
+print(defaultChangeParam())  # ['END', 'END', 'END']
