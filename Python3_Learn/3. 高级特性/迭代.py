fdf076b6d13535afebba78251ad482e841afec2c
nodcat <17089793114@163.com>
2017-08-29 23:57:11 +0800

    修改

--- Python3_Learn/3. 高级特性/迭代.py
+++ Python3_Learn/3. 高级特性/迭代.py
@@ -1,16 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+如果给定一个list或tuple，我们可以通过for循环来遍历这个list或tuple，这种遍历我们称为迭代（Iteration）。
+
+在Python中，迭代是通过for ... in来完成的，而很多语言比如C或者Java，迭代list是通过下标完成的，比如Java代码：
+
+for (i=0; i<list.length; i++) {
+    n = list[i];
+}
+可以看出，Python的for循环抽象程度要高于Java的for循环，因为Python的for循环不仅可以用在list或tuple上，还可以作用在其他可迭代对象上。
+
+list这种数据类型虽然有下标，但很多其他数据类型是没有下标的，但是，只要是可迭代对象，无论有无下标，都可以迭代，比如dict就可以迭代
+
+因为dict的存储不是按照list的方式顺序排列，所以，迭代出的结果顺序很可能不一样。
+
+
+
+猜想：python中实现了__iter__方法和__next__方法的对象都是可迭代对象?
+
+'''
+
 from collections import Iterable, Iterator
+
 
 def g():
     yield 1
     yield 2
     yield 3
 
+'''
+我们已经知道，可以直接作用于for循环的数据类型有以下几种：
+
+一类是集合数据类型，如list、tuple、dict、set、str等；
+
+一类是generator，包括生成器和带yield的generator function。
+
+这些可以直接作用于for循环的对象统称为可迭代对象：Iterable。
+
+可以使用isinstance()判断一个对象是否是Iterable对象：
+'''
+
+# 列表可迭代
 print('Iterable? [1, 2, 3]:', isinstance([1, 2, 3], Iterable))
+# 字符串可迭代
 print('Iterable? \'abc\':', isinstance('abc', Iterable))
+# 整数不可迭代
 print('Iterable? 123:', isinstance(123, Iterable))
+# 生成器可迭代
 print('Iterable? g():', isinstance(g(), Iterable))
 
 print('Iterator? [1, 2, 3]:', isinstance([1, 2, 3], Iterator))
