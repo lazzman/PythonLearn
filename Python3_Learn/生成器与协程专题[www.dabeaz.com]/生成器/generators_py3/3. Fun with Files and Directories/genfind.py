2b3ddb2a1aaf04bea8ac00edebec046d4d40424f
NightGodCat <17089793114@163.com>
2017-10-09 10:22:14 +0800

    修改

---
+++ Python3_Learn/生成器与协程专题[www.dabeaz.com]/生成器/generators_py3/3. Fun with Files and Directories/genfind.py
@@ -0,0 +1,19 @@
+# genfind.py
+# 生成器-返回匹配模式的文件名序列
+# A function that generates files that match a given filename pattern
+
+import os
+import fnmatch
+
+
+def gen_find(filepat, top):
+    for path, dirlist, filelist in os.walk(top):
+        for name in fnmatch.filter(filelist, filepat):
+            yield os.path.join(path, name)
+
+
+# Example use
+if __name__ == '__main__':
+    lognames = gen_find("access-log*", "www")
+    for name in lognames:
+        print(name)
