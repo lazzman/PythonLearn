c7958661f93581a7fddd4b6b4073f35179e44c36
nodcat <17089793114@163.com>
2017-10-17 21:59:38 +0800

    修改

--- Python3_Learn/4.函数式编程/prime_numbers.py
+++ Python3_Learn/4.函数式编程/prime_numbers.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+# 1000以内的质数
+
+
 def main():
     for n in primes():
         if n < 1000:
@@ -8,14 +12,17 @@ def main():
         else:
             break
 
+
 def _odd_iter():
     n = 1
     while True:
         n = n + 2
         yield n
 
+
 def _not_divisible(n):
     return lambda x: x % n > 0
+
 
 def primes():
     yield 2
@@ -24,6 +31,7 @@ def primes():
         n = next(it)
         yield n
         it = filter(_not_divisible(n), it)
+
 
 if __name__ == '__main__':
     main()
