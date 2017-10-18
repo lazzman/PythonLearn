e7dd86c0b45d33d7a7f98600aed42501dfcd542c
nodcat <17089793114@163.com>
2017-10-18 22:36:42 +0800

    修改

---
+++ Python3_Learn/7. 面向对象编程高级/MixIn组合方式.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'在设计类的继承关系时，通常，主线都是单一继承下来的，例如，Ostrich继承自Bird。但是，如果需要“混入”额外的功能，通过多重继承就可以实现，比如，让Ostrich除了继承自Bird外，再同时继承Runnable。这种设计通常称之为MixIn。'
+
+'''
+为了更好地看出继承关系，我们把Runnable和Flyable改为RunnableMixIn和FlyableMixIn。类似的，你还可以定义出肉食动物CarnivorousMixIn和植食动物HerbivoresMixIn，让某个动物同时拥有好几个MixIn：
+------------------------------------------------------
+class Dog(Mammal, RunnableMixIn, CarnivorousMixIn):
+    pass
+------------------------------------------------------
+
+!!! MixIn的目的就是给一个类增加多个功能，这样，在设计类的时候，我们优先考虑通过多重继承来组合多个MixIn的功能，而不是设计多层次的复杂的继承关系。
+
+Python自带的很多库也使用了MixIn。举个例子，Python自带了TCPServer和UDPServer这两类网络服务，而要同时服务多个用户就必须使用多进程或多线程模型，这两种模型由ForkingMixIn和ThreadingMixIn提供。通过组合，我们就可以创造出合适的服务来。
+
+比如，编写一个多进程模式的TCP服务，定义如下：
+------------------------------------------------------
+class MyTCPServer(TCPServer, ForkingMixIn):
+    pass
+------------------------------------------------------
+
+编写一个多线程模式的UDP服务，定义如下：
+------------------------------------------------------
+class MyUDPServer(UDPServer, ThreadingMixIn):
+    pass
+------------------------------------------------------
+
+如果你打算搞一个更先进的协程模型，可以编写一个CoroutineMixIn：
+------------------------------------------------------
+class MyTCPServer(TCPServer, CoroutineMixIn):
+    pass
+------------------------------------------------------
+
+这样一来，我们不需要复杂而庞大的继承链，只要选择组合不同的类的功能，就可以快速构造出所需的子类。
+
+由于Python允许使用多重继承，因此，MixIn就是一种常见的设计。
+
+只允许单一继承的语言（如Java）不能使用MixIn的设计。
+'''
---
+++ Python3_Learn/7. 面向对象编程高级/复杂继承成员解析顺序.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'由于Python是多继承，那么很容易想到一个问题，继承顺序是如何？'
+
+'''
+mro是method resolution order的缩写，表示了类继承体系中的成员解析顺序。
+
+注意：python2与python3的mro策略有所不同，这里我们只看Python3的mro策略
+
+在python中，每个类都有一个mro的类方法。
+---------------------------------------------
+class Base(object):
+	def __init__(self):
+		print “Base init”
+class Medium1(Base):
+	def __init__(self):
+		super(Medium1, self).__init__()
+		print “Medium1 init”
+class Medium2(Base):
+	def __init__(self):
+		super(Medium2, self).__init__()
+		print “Medium2 init”
+class Leaf(Medium1, Medium2):
+	def __init__(self):
+		super(Leaf, self).__init__()
+		print “Leaf init”
+>>> Leaf.mro()
+[<class '__main__.Leaf'>, <class '__main__.Medium1'>, <class '__main__.Base'>, <class '__main__.Medium2'>, <type 'object'>]
+---------------------------------------------
+
+mro方法返回的是一个祖先类的列表。Leaf的每个祖先都在其中出现一次，这也是super在父类中查找成员的顺序。
+
+通过mro，python巧妙地将多继承的图结构，转变为list的顺序结构。super在继承体系中向上的查找过程，变成了在mro中向右的线性查找过程，任何类都只会被处理一次。
+
+通过这个方法，python解决了多继承中的2大难题：
+
+1. 查找顺序问题。从Leaf的mro顺序可以看出，如果Leaf类通过super来访问父类成员，那么Medium1的成员会在Medium2之前被首先访问到。如果Medium1和Medium2都没有找到，最后再到Base中查找。
+
+2. 钻石继承的多次初始化问题。在mro的list中，Base类只出现了一次。事实上任何类都只会在mro list中出现一次。这就确保了super向上调用的过程中，任何祖先类的方法都只会被执行一次。
+
+至于mro的生成算法，可以参考这篇wiki：https://en.wikipedia.org/wiki/C3_linearization
+'''
+
+
+class D(object):
+    def run(self):
+        print('d')
+
+    def sing(self):
+        print('d')
+
+
+class E(object):
+    def run(self):
+        print('e')
+
+    def sing(self):
+        print('e')
+
+    def look(self):
+        print('e')
+
+
+class F(object):
+    def run(self):
+        print('f')
+
+    def sing(self):
+        print('f')
+
+
+class C(D, F):
+    def run(self):
+        print('c')
+
+    def sing(self):
+        print('c')
+
+    def look(self):
+        print('c')
+
+
+class B(E, D):
+    pass
+
+
+class A(B, C):
+    pass
+
+
+# 按照继承顺序，查找run()方法时顺序是：B->E->C->D->F
+print(A.mro())
+a = A()
+a.run()
+
+# 按照继承顺序，查找run()方法时顺序是：B->E->C->D->F
+a.look()
+
+# 按照继承顺序,查找sing()方法的顺序是：B->E->D
+print(B.mro())
+b = B()
+b.sing()
