9cbd7fe16bc1a7306d3ab5c713acf0ccf8dbdc00
nodcat <17089793114@163.com>
2017-10-22 22:51:29 +0800

    修改

--- Python3_Learn/18. Web开发/do_flask.py
+++ Python3_Learn/18. Web开发/do_flask.py
@@ -6,9 +6,11 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return '<h1>Home</h1>'
+
 
 @app.route('/signin', methods=['GET'])
 def signin_form():
@@ -18,12 +20,14 @@ def signin_form():
               <p><button type="submit">Sign In</button></p>
               </form>'''
 
+
 @app.route('/signin', methods=['POST'])
 def signin():
     # 需要从request对象读取表单内容：
-    if request.form['username']=='admin' and request.form['password']=='password':
+    if request.form['username'] == 'admin' and request.form['password'] == 'password':
         return '<h3>Hello, admin!</h3>'
     return '<h3>Bad username or password.</h3>'
+
 
 if __name__ == '__main__':
     app.run()
