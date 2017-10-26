2238e6a1b3f080fabd18db5aae61cb5ebc5af4dd
nodcat <17089793114@163.com>
2017-10-26 23:11:05 +0800

    修改

--- Python3_Learn/18. Web开发/do_wsgi.py
+++ Python3_Learn/18. Web开发/do_wsgi.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+
 from wsgiref.simple_server import make_server
 
 from hello import application
 
+# WSGI仅仅将请求处理传递给application函数，如果application函数中发生阻塞，那么后面的请求也将无法处理
 httpd = make_server('', 8000, application)
 print('Serving HTTP on port 8000...')
 
--- Python3_Learn/18. Web开发/hello.py
+++ Python3_Learn/18. Web开发/hello.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
+
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html; charset=UTF-8')])
     body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or '18. Web开发')
+    if environ['PATH_INFO'] == '/sleep':
+        time.sleep(20)
     return [body.encode('utf-8')]
