19fa2b9c42c816551d7987641cb737f56abeac6e
nodcat <17089793114@163.com>
2017-10-26 22:55:31 +0800

    修改

--- Python3_Learn/18. Web开发/hello.py
+++ Python3_Learn/18. Web开发/hello.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response('200 OK', [('Content-Type', 'text/html; charset=UTF-8')])
     body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or '18. Web开发')
     return [body.encode('utf-8')]
--- Python3_Learn/3. 高级特性/yield from.py
+++ Python3_Learn/3. 高级特性/yield from.py
@@ -5,26 +5,31 @@
 def each_ascii(s):
     for ch in s:
         yield ord(ch)
-    return "len('"+ s + "')= %s" % len(s)
+    # return语句实际上会抛出StopIteration() 异常，所以return的值会成为调用者中yield from表达式的返回值。
+    return "len('" + s + "')= %s" % len(s)
+
 
 def yield_from(s):
+    # r的返回值是由each_ascii(s)中迭代结束时抛出的StopIteration异常的第一个参数（即迭代器中return 的值）。
     r = yield from each_ascii(s)
     print(r)
 
+
 def main():
     for x in each_ascii('abc'):
-        print(x) # => 'a', 'b', 'c'
+        print(x)  # => 'a', 'b', 'c'
     it = each_ascii('xyz')
     try:
         while True:
-            print(next(it)) # => 'x', 'y', 'z'
+            print(next(it))  # => 'x', 'y', 'z'
     except StopIteration as s:
-        print(s.value) # => '3 chars'
+        print(s.value)  # => '3 chars'
 
     # using yield from in main() will change main() from 2. 函数 to generator:
     # r = yield from each_ascii('hello')
 
     for ch in yield_from('hello'):
         pass
+
 
 main()
