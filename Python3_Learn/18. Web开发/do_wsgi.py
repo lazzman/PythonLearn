6b245a083f35992eb345988c8b311ee5ce9427c1
nodcat <17089793114@163.com>
2017-10-29 22:55:58 +0800

    修改

--- Python3_Learn/18. Web开发/do_wsgi.py
+++ Python3_Learn/18. Web开发/do_wsgi.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-
+import time
 from wsgiref.simple_server import make_server
 
-from hello import application
 
-# WSGI仅仅将请求处理传递给application函数，如果application函数中发生阻塞，那么后面的请求也将无法处理
+# 定义实现WSGI接口规范的函数
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html; charset=UTF-8')])
+    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or '18. Web开发')
+    if environ['PATH_INFO'] == '/sleep':
+        time.sleep(20)
+    return [body.encode('utf-8')]
+
+
+# WSGI仅仅将全部请求处理传递给application函数，如果application函数中发生阻塞，那么后面的请求也将无法处理
 httpd = make_server('', 8000, application)
 print('Serving HTTP on port 8000...')
 
